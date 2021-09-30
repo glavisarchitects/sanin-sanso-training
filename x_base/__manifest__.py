8526d6008910fcea3d7a011c44cf4455ece6108a
SGVNAnhLD <anhld@systemgear-vietnam.com>
2021-09-30 12:02:40 +0700

    [ADD] PO and Rebate

---
+++ x_base/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Code Base",
+    "summary": """
+        Code Base
+    """,
+    "version": "14.0.1.0.0",
+    "category": "Purchase",
+    "depends": [
+        "x_company_organization", "x_partner"
+    ],
+    "data": [
+        # SECURITY
+        "security/ir.model.access.csv",
+        # DATA
+        # VIEWS
+        "views/web_access_rule_buttons.xml",
+        # REPORTS
+        "reports/layout.xml",
+        "reports/paperformat.xml",
+
+    ],
+    "application": False,
+    "installable": True,
+    'pre_init_hook': 'pre_init_hook',
+    'uninstall_hook': 'uninstall_hook',
+
+}
---
+++ x_base/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import transaction_classification
+from . import mail_mail
+from . import model
+from . import res_organization
