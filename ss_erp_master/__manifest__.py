a0b503752091d5d20859fe1059d8da1980e83652
SGVNAnhLD <anhld@systemgear-vietnam.com>
2021-10-12 14:13:14 +0700

    [ADD] ADD NEW MASTER STRUCTURE
    1. 組織
    2. 組織カテゴリ
    3. 管轄部門

---
+++ ss_erp_master/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "SS ERP Master",
+    "summary": """
+        SS ERP Master
+    """,
+    "version": "14.0.1.0.0",
+    "category": "",
+    "depends": [
+        "mail"
+    ],
+    "data": [
+        # SECURITY
+        "security/ir.model.access.csv",
+        # DATA
+        # VIEWS
+        "views/responsible_department_views.xml",
+        "views/organization_views.xml",
+        "views/organization_category_views.xml",
+        # REPORTS
+
+    ],
+    "application": False,
+    "installable": True,
+}
