104d67be87fe6c127a11d117dd4eda640f674eb0
SGVNAnhLD <anhld@systemgear-vietnam.com>
2021-11-04 13:37:18 +0700

    [Update] Update Sale order

--- ss_erp/models/sale_order.py
+++ ss_erp/models/sale_order.py
@@ -7,8 +7,8 @@ from odoo.exceptions import UserError
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    x_organization_id = fields.Many2one('ss_erp.organization', string="Organization in charge", copy=False)
-    x_responsible_dept_id = fields.Many2one('ss_erp.responsible.department', string="Responsible Dept", copy=False)
+    x_organization_id = fields.Many2one('ss_erp.organization', string="販売支店", copy=False)
+    x_responsible_dept_id = fields.Many2one('ss_erp.responsible.department', string="管轄部門", copy=False)
 
     # TODO: Need re confirm  x_import_id
     x_import_id = fields.Char("Import ID")
@@ -18,5 +18,5 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     # TODO: Need re check
-    x_pricelist = fields.Many2one('product.pricelist', string="Product Price", index=True)
-    x_validate_date = fields.Date("Validate Date")
+    x_pricelist = fields.Many2one('product.pricelist', string="価格リスト", index=True)
+    x_expected_delivery_date = fields.Date("納期予定日")
