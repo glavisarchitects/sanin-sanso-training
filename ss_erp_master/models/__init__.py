70df99d3bfc14cce9c8b8531ea00d82baa8f9cba
SGVNAnhLD <anhld@systemgear-vietnam.com>
2021-11-11 16:41:54 +0700

    [UPDATE] Remove old module

--- ss_erp_master/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import responsible_department
-from . import organization
-from . import organization_category
-from . import res_company
-from . import res_partner
-from . import ss_erp_bis_category
