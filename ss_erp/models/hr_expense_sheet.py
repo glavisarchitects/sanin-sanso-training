c070ced0ef5396b9b6481346601b3c5339151f9d
SGVNAnhLD <anhld@systemgear-vietnam.com>
2022-02-11 18:02:30 +0700

    [Update] 経費画面に組織及び管轄部門のデフォルトを追加

--- ss_erp/models/hr_expense_sheet.py
+++ ss_erp/models/hr_expense_sheet.py
@@ -7,8 +7,8 @@ class HrExpenseSheet(models.Model):
 
     x_sub_account_id = fields.Many2one('ss_erp.account.subaccount', string='補助科目')
     x_request_date = fields.Date(string='申請日')
-    x_organization_id = fields.Many2one('ss_erp.organization', string='申請組織')
-    x_responsible_id = fields.Many2one('ss_erp.responsible.department', string='申請部署')
+    x_organization_id = fields.Many2one('ss_erp.organization', string='申請組織',default=lambda self: self._get_default_x_organization_id())
+    x_responsible_id = fields.Many2one('ss_erp.responsible.department', string='申請部署',default=lambda self: self._get_default_x_responsible_dept_id())
 
     @api.onchange('account_id')
     def onchange_sub_account_id(self):
@@ -16,3 +16,17 @@ class HrExpenseSheet(models.Model):
             sub_accounts = self.account_id.x_sub_account_ids.ids
             return {'domain': {'x_sub_account_id': [('id', 'in', sub_accounts)]
                                }}
+
+    def _get_default_x_organization_id(self):
+        employee_id = self.env['hr.employee'].search([('user_id', '=', self.env.user.id)], limit=1)
+        if employee_id:
+            return employee_id.organization_first
+        else:
+            return False
+
+    def _get_default_x_responsible_dept_id(self):
+        employee_id = self.env['hr.employee'].search([('user_id', '=', self.env.user.id)], limit=1)
+        if employee_id:
+            return employee_id.department_jurisdiction_first
+        else:
+            return False
--- ss_erp/models/ss_erp_hr_internal_note.py
+++ ss_erp/models/ss_erp_hr_internal_note.py
@@ -6,11 +6,16 @@ class HrInternalNote(models.Model):
     _name = "ss_erp.hr.internal.note"
     _description = "支店入出金ノート"
 
-    organization_id = fields.Many2one('ss_erp.organization', string='組織名', index=True)
+    organization_id = fields.Many2one('ss_erp.organization', string='組織名', index=True,default=lambda self: self._get_default_x_organization_id())
     name = fields.Char('メモ')
     ref = fields.Many2many('hr.expense.sheet', string='参照元')
 
-
+    def _get_default_x_organization_id(self):
+        employee_id = self.env['hr.employee'].search([('user_id', '=', self.env.user.id)], limit=1)
+        if employee_id:
+            return employee_id.organization_first
+        else:
+            return False
 
 
 
