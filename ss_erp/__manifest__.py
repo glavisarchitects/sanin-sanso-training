b01fcb728f09919f47f6d198c05fd1af1777b24f
SGVNAnhLD <anhld@systemgear-vietnam.com>
2022-02-23 14:03:03 +0700

    Update __manifest__.py

--- ss_erp/__manifest__.py
+++ ss_erp/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "14.0.1.9",
     "category": "",
     "depends": [
-        "base", "delivery", "mail", "contacts", "purchase", "uom",
+        "base", "mail", "contacts", "purchase", "uom",
         "purchase_stock", "web", "account", "stock_dropshipping", "approvals", "hr",
         "approvals_purchase_stock", "base_import","hr_expense", "sale_expense"
     ],
