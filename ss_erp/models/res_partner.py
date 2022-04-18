13f9283c5d45de6e977c2d6313fe17107f0c8e08
SGVNAnhLD <anhld@systemgear-vietnam.com>
2022-04-18 10:02:33 +0700

    FIX BUG

--- ss_erp/models/res_partner.py
+++ ss_erp/models/res_partner.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
+from odoo.models import NewId
 
 
 class ResPartner(models.Model):
@@ -173,6 +174,7 @@ class ResPartner(models.Model):
                 if len(partner) > 1:
                     raise ValidationError(_("申請対象の取引先は、顧客または仕入先として既に登録済みの可能性があります。"))
 
+
     @api.constrains('x_transaction_categ')
     def _check_transaction_categ(self):
         for record in self:
@@ -223,3 +225,17 @@ class ResPartner(models.Model):
                 values.update({field_name: value})
             form_id.write(values)
         return res
+
+    def check_condition_show_dialog(self, vals=False, data_changed=False):
+        if not any([vals.get('zip'),vals.get('state_id'),vals.get('city'),vals.get('street'),vals.get('street2')]):
+            return False
+        dup_add = [('zip', '=', vals.get('zip')), ('state_id', '=', vals.get('state_id')),
+             ('city', '=', vals.get('city')), ('street', '=', vals.get('street')),
+             ('street2', '=', vals.get('street2'))]
+        if not isinstance(self.id, NewId):
+            dup_add.append(('id', '!=', self.id))
+        address_partner_fields = self.search(dup_add)
+        if address_partner_fields:
+            return True
+        return False
+
---
+++ ss_erp/static/src/js/confirm_on_save.js
@@ -0,0 +1,69 @@
+odoo.define('ss_erp.web_confirm_on_save', function (require) {
+"use strict";
+
+    var ajax = require('web.ajax');
+    var AbstractView = require('web.AbstractView');
+    var FormController = require('web.FormController');
+    var Dialog = require('web.Dialog');
+    var session = require('web.session');
+
+AbstractView.include({
+
+	init: function (viewInfo, params) {
+    	var self = this;
+    	this._super.apply(this, arguments);
+    	var confirm =  this.arch.attrs.confirm_partner_custom ? this.arch.attrs.confirm_partner_custom : false;
+    	self.controllerParams.activeActions.confirm_partner_custom = confirm;
+
+    },
+
+});
+
+FormController.include({
+
+	check_condition: function (modelName, vals ,data_changed) {
+        var def = this._rpc({
+            "model": modelName,
+            "method": "check_condition_show_dialog",
+            "args": [vals ,data_changed]
+        });
+        return def;
+    },
+
+	_onSave: function (ev) {
+		var self = this;
+		var modelName = this.modelName ? this.modelName : false;
+		var record = this.model.get(this.handle, {raw: true});
+		var data_changed = record ? record.data : false;
+		var vals = data_changed && data_changed.id ? data_changed.id : false;
+		var confirm = self.activeActions.confirm_partner_custom;
+
+		function saveAndExecuteAction () {
+			ev.stopPropagation(); // Prevent x2m lines to be auto-saved
+			self._disableButtons();
+			self.saveRecord().then(self._enableButtons.bind(self)).guardedCatch(self._enableButtons.bind(self));
+	    }
+		if( modelName ==='res.partner' || modelName ==='ss_erp.res.partner.form' && confirm){
+			self.check_condition(modelName, vals, data_changed).then(function(opendialog){
+	        	if(!opendialog){
+	        		saveAndExecuteAction();
+	        	}else{
+	        		if(confirm && opendialog){
+	        			var def = new Promise(function (resolve, reject) {
+	        	            Dialog.confirm(self, confirm, {
+	        	                confirm_callback: saveAndExecuteAction,
+	        	            }).on("closed", null, resolve);
+	        	        });
+	        		}else{
+	        			saveAndExecuteAction();
+	        		}
+	        	}
+	        });
+		}else{
+			saveAndExecuteAction();
+		}
+	},
+
+});
+
+});
