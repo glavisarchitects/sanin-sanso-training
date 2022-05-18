34e74adab6d14040238c7d081360572d5f3f6279
SGVNAnhLD <anhld@systemgear-vietnam.com>
2022-05-18 11:52:11 +0700

    FIX BUG

--- ss_erp/models/approval_category.py
+++ ss_erp/models/approval_category.py
@@ -57,7 +57,7 @@ class ApprovalCategory(models.Model):
     has_x_transfer_date = fields.Selection(
         CATEGORY_SELECTION, string="送金日", default="no", )
     has_x_inventory_instruction_ids = fields.Selection(
-        CATEGORY_SELECTION, string="指示伝票", default="no", )
+        CATEGORY_SELECTION, string="棚卸指示伝票", default="no", )
     approval_type = fields.Selection(selection_add=[
         ('inventory_request', '棚卸'),
         ('inventory_request_manager', '棚卸マネージャー'),
