84a9f92f38bffe5b9763bc4d30756b10640305fc
SGVNAnhLD <anhld@systemgear-vietnam.com>
2022-06-30 09:31:12 +0700

    Delete 棚卸種別

--- ss_erp/models/stock_inventory_line.py
+++ ss_erp/models/stock_inventory_line.py
@@ -12,7 +12,7 @@ class StockInventoryLine(models.Model):
     organization_id = fields.Many2one(
         'ss_erp.organization', string='組織名',
         related='inventory_order_line_id.organization_id')
-    type_id = fields.Many2one(related='inventory_order_line_id.type_id', string='棚卸種別')
+    # type_id = fields.Many2one(related='inventory_order_line_id.type_id', string='棚卸種別')
     product_cost = fields.Float(string='単価')
 
     def write(self, vals):
