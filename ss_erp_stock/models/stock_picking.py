b7ab8ac65a007658f0cf7c98d7b2f28fc17ab736
SGVNAnhLD <anhld@systemgear-vietnam.com>
2022-09-21 15:12:02 +0700

    FIX Stock

--- ss_erp_stock/models/stock_picking.py
+++ ss_erp_stock/models/stock_picking.py
@@ -13,11 +13,15 @@ class StockPicking(models.Model):
         "在庫仕訳訂正", index=True)
     x_dest_address_info = fields.Html("直送先住所")
     x_organization_id = fields.Many2one(
-        'ss_erp.organization', string="移動元組織", domain="[('warehouse_id','!=',False)]",
-                                        default=lambda self: self._get_default_x_organization_id())
+        'ss_erp.organization', string="移動元組織", domain=lambda self: [('id', 'in', self._login_user_organization_id())],
+        default=lambda self: self._get_default_x_organization_id())
     x_responsible_dept_id = fields.Many2one(
         'ss_erp.responsible.department', string="移動元管轄部門",
-                                            default=lambda self: self._get_default_x_responsible_dept_id())
+        default=lambda self: self._get_default_x_responsible_dept_id())
+
+    def _login_user_organization_id(self):
+        organization_ids = self.env.user.organization_ids.filtered(lambda x: x.warehouse_id != False)
+        return organization_ids.ids if organization_ids else False
 
     def _get_default_x_organization_id(self):
         employee_id = self.env['hr.employee'].sudo().search([('user_id', '=', self.env.user.id)], limit=1)
@@ -32,13 +36,6 @@ class StockPicking(models.Model):
             return employee_id.department_jurisdiction_first
         else:
             return False
-
-    login_user_organization_ids = fields.Many2many('ss_erp.organization',
-                                                   compute='_compute_login_user_organization_ids')
-
-    def _compute_login_user_organization_ids(self):
-        for rec in self:
-            rec.login_user_organization_ids = self.env.user.organization_ids.ids
 
     x_responsible_dept_dest_id = fields.Many2one('ss_erp.responsible.department', string='移動先管轄部門', store=True)
     x_organization_dest_id = fields.Many2one('ss_erp.organization', string='移動先組織', store=True)
@@ -95,15 +92,20 @@ class StockPicking(models.Model):
     @api.onchange('picking_type_id')
     def _onchange_picking_type_id(self):
         if self.picking_type_code == 'incoming':
-            return {'domain': {'location_dest_id': [('usage','=','internal'),('id','child_of',self.picking_type_id.warehouse_id.view_location_id.id)]}}
+            return {'domain': {'location_dest_id': [('usage', '=', 'internal'), (
+            'id', 'child_of', self.picking_type_id.warehouse_id.view_location_id.id)]}}
         elif self.picking_type_code == 'outgoing':
-            return {'domain': {'location_id': [('usage','=','internal'),('id','child_of',self.picking_type_id.warehouse_id.view_location_id.id)]}}
+            return {'domain': {'location_id': [('usage', '=', 'internal'), (
+            'id', 'child_of', self.picking_type_id.warehouse_id.view_location_id.id)]}}
         elif self.picking_type_code == 'internal':
-            return {'domain': {'location_dest_id': [('usage','=','internal'),('id','child_of',self.picking_type_id.warehouse_id.view_location_id.id)],
-                               'location_id': [('usage','=','internal'),('id','child_of',self.picking_type_id.warehouse_id.view_location_id.id)]}}
+            return {'domain': {'location_dest_id': [('usage', '=', 'internal'), (
+            'id', 'child_of', self.picking_type_id.warehouse_id.view_location_id.id)],
+                               'location_id': [('usage', '=', 'internal'), (
+                               'id', 'child_of', self.picking_type_id.warehouse_id.view_location_id.id)]}}
         elif self.picking_type_code == 'mrp_operation':
-            return {'domain': {'location_id': [('usage','=','internal'),('id','child_of',self.picking_type_id.warehouse_id.view_location_id.id)]}
-                               }
+            return {'domain': {'location_id': [('usage', '=', 'internal'), (
+            'id', 'child_of', self.picking_type_id.warehouse_id.view_location_id.id)]}
+                    }
 
 
 class StockMove(models.Model):
