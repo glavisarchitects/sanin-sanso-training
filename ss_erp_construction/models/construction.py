c7674b2b2092e9d0297e622e2aeaab7f2a41360d
SGVNAnhLD <anhld@systemgear-vietnam.com>
2022-10-24 09:00:03 +0700

    Update construction.py

--- ss_erp_construction/models/construction.py
+++ ss_erp_construction/models/construction.py
@@ -552,7 +552,7 @@ class Construction(models.Model):
             stock_picking.action_assign()
 
     def action_picking_from_warehouse(self):
-        if not self.construction_component_ids.filtered(lambda x:x.qty_to_buy !=0):
+        if not self.construction_component_ids.filtered(lambda x:x.qty_to_buy !=0 and x.product_id.type == 'product'):
             raise UserError('出荷するものは何もありません！')
         else:
             self._prepare_stock_picking()
