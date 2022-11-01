b07d33a02bf33c04cf84ea8c3b552a852817a26f
SGVNAnhLD <anhld@systemgear-vietnam.com>
2022-11-01 16:38:04 +0700

    Update construction

--- ss_erp_construction/models/approval_category.py
+++ ss_erp_construction/models/approval_category.py
@@ -12,7 +12,7 @@ class ApprovalCategory(models.Model):
     _inherit = 'approval.category'
 
     has_construction_order_id = fields.Selection(
-        CATEGORY_SELECTION, string="工事オーダー", default="no", )
+        CATEGORY_SELECTION, string="工事オーダ", default="no", )
     has_construction_order_id2 = fields.Selection(
         CATEGORY_SELECTION, string="工事見積書", default="no", )
     has_construction_template_id = fields.Selection(
