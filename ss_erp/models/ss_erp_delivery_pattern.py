e3a334f37e725dfba0685ac05690c403a526f432
SGVNAnhLD <anhld@systemgear-vietnam.com>
2022-12-14 11:16:47 +0700

    Delete unnecessary module

--- ss_erp/models/ss_erp_delivery_pattern.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-
-class DeliveryPattern(models.Model):
-    _name = 'ss_erp.delivery.pattern'
-    _description = '配信パターン'
-
-    name = fields.Char(string='配信パターン')
